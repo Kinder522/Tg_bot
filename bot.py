9381d974ad3c30bb82767a515a792c13f799f79a
Виноградов Вячеслав <slava.vinogradov.2009@mail.ru>
2025-02-09 19:21:09 +0300

    working

--- bot.py
+++ bot.py
@@ -2,24 +2,62 @@ from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
 from aiogram.types import Message
 from Config import BOT_TOKEN
-
+import random
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
-
-
+num = 0
+wait = False
+wait_to_ans = False
 @dp.message(Command(commands=["start"]))
 async def start(message: Message):
-    print(message)
-    await message.answer("Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь!")
+    global wait
+    await message.answer("Привет!Я бот который играет  с тобой угадай число\n, я загадываю люьое число от 1 до 10 и говорю отгадал ты или нет,ты готов?")
+    wait = True
+    
+    
 
 @dp.message(Command(commands=["help"]))
 async def help(message: Message):
     await message.answer(
         '''Этот раздел времено недоступен,перезвоните позже'''
         )
-answer = False
+
+
 @dp.message()
 async def send_echo(message: Message):
-    await message.reply(text=message.text)
+    if(wait_to_ans == False):
+        if (wait == True):
+            await number(message)
+    else:
+        await answer(message)
+
+
+    
+
+async def ready(func):
+    async def wrapper(message: Message):
+        global wait
+        if message.text.lower() == "да":
+            wait = False
+            func(message)  
+    return wrapper
+
+
+async def answer(message:Message):
+    global wait_to_ans
+    if(message.text == str(num)):
+        await message.reply("Молодец,ты угадал!!🎉🎉❤")
+        wait_to_ans = False
+    else:
+        await message.reply("Не угадал😢😢,попробуй еще")
+
+
+
+@ready
+async def number(message : Message):
+    global wait_to_ans,num
+    num = random.randint(1,10)
+    await message.answer("все я загадал,угадывай число")
+    wait_to_ans = True
 
 dp.run_polling(bot)
