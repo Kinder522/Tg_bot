ae17bd84dd64d8cc7d5db521f13f28ee42500d7c
Виноградов Вячеслав <slava.vinogradov.2009@mail.ru>
2025-02-19 20:26:49 +0300

    почти рабочий бот

--- bot.py
+++ bot.py
@@ -1,63 +1,76 @@
-from aiogram import Bot, Dispatcher
+from aiogram import F, Bot, Dispatcher
 from aiogram.filters import Command
 from aiogram.types import Message
 from Config import BOT_TOKEN
 import random
+
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 num = 0
 wait = False
 wait_to_ans = False
+
+user = {'in_game': False,
+        'secret_number': None,
+        'attempts': 5,
+        'total_games': 0,
+        'wins': 0}
+
+
 @dp.message(Command(commands=["start"]))
 async def start(message: Message):
     global wait
-    await message.answer("Привет!Я бот который играет  с тобой угадай число\n, я загадываю люьое число от 1 до 10 и говорю отгадал ты или нет,ты готов?")
-    wait = True
+    await message.answer(
+        "Привет!Я бот который играет  с тобой угадай число\n, я загадываю любое число от 1 до 10 и говорю отгадал ты или нет,ты готов?")
     
-    
+
 
 @dp.message(Command(commands=["help"]))
 async def help(message: Message):
     await message.answer(
         '''Этот раздел времено недоступен,перезвоните позже'''
-        )
+    )
 
+@dp.message(Command(commands="stat"))
+async def process_stat_command(message: Message):
+    await message.answer(f"🎯Доступные попытки: {user['attempts']}(с платной версией 10)\n🎉Всего игр сыграно: {user['total_games']} \n🏆Побед: {user['wins']}")
+
+
+@dp.message(Command(commands="cancel"))
+async def process_cancel_command(message: Message):
+    global user
+    if user['in_game']:
+        user['in_game'] = False
+        await message.answer("Эммм нуу окей,как хочешь")
+    else:
+        await message.answer("Чо ты там отменить пытаешься тупой,эволюцию себе отмени❤❤❤")
+
+@dp.message(F.text.lower().in_(['да', 'давай', 'сыграем', 'игра', 'играть', 'хочу играть']))
+async def process_positive_answer(message: Message):
+    global user,num
+    user['in_game'] = True
+    num = randomNum()
+    await message.answer("Все я загадал,можешь отгадывать")
+
+def randomNum() -> int:
+    return random.randint(1,10)
 
 @dp.message()
-async def send_echo(message: Message):
-    if(wait_to_ans == False):
-        if (wait == True):
-            await number(message)
-    else:
-        await answer(message)
-
-
-    
-
-async def ready(func):
-    async def wrapper(message: Message):
-        global wait
-        if message.text.lower() == "да":
-            wait = False
-            func(message)  
-    return wrapper
-
-
 async def answer(message:Message):
-    global wait_to_ans
-    if(message.text == str(num)):
-        await message.reply("Молодец,ты угадал!!🎉🎉❤")
-        wait_to_ans = False
-    else:
-        await message.reply("Не угадал😢😢,попробуй еще")
-
-
-
-@ready
-async def number(message : Message):
-    global wait_to_ans,num
-    num = random.randint(1,10)
-    await message.answer("все я загадал,угадывай число")
-    wait_to_ans = True
+    global num
+    if user['in_game']:
+        if message.text.isdigit():
+            if int(message.text) >= 0 and int(message.text) <= 100:
+                if user['attempts'] > 0:
+                    if int(message.text) == num :
+                        await message.answer("Молодец,угадал,просто тигр❤❤❤")
+                        user['in_game'] = False
+                    else:
+                        await message.answer("Не угадал,попробуй еще😢😢😢")
+                        user['attempts'] -= 1
+                else:
+                    await message.answer("У тебя попыток не осталось,ТУПОЙ")
+        else:
+            await message.reply("А ну говори нормальным языком,ничего же не понятно")
 
 dp.run_polling(bot)
--- test2.py
+++ test2.py
@@ -1,4 +1,28 @@
-def say_something(number, word):
+class MyClass:
+    def __init__(self, a: int, b: str):
+        self.a = a
+        self.b = b
+
+    def get_a(self) -> int:
+        return self.a
+
+    def set_a(self, a: int):
+        self.a = a
+
+    def get_b(self) -> str:
+        return self.b
+
+    def set_b(self, b: str):
+        self.b = b
+
+
+def say_something(number: int, word: str) -> str:
     word = word.capitalize()
     return word * number
-  
+
+
+m = MyClass(3, "heLLo")
+print(say_something(m.get_a(), m.get_b()))
+m.set_a("dsa")
+m.set_b("TRY")
+print(say_something(m.get_a(), m.get_b()))
