301c02d1829f1134d4181665345996770c9e2f8c
Виноградов Вячеслав <slava.vinogradov.2009@mail.ru>
2025-02-23 20:14:39 +0300

    добавил что бы нормально работал когда бот использует сразу несколько человек

--- bot.py
+++ bot.py
@@ -10,18 +10,25 @@ num = 0
 wait = False
 wait_to_ans = False
 
-user = {'in_game': False,
-        'secret_number': None,
-        'attempts': 5,
-        'total_games': 0,
-        'wins': 0}
+
+users = {}
 
 
 @dp.message(Command(commands=["start"]))
 async def start(message: Message):
     global wait
-    await message.answer(
-        "Привет!Я бот который играет  с тобой угадай число\n, я загадываю любое число от 1 до 10 и говорю отгадал ты или нет,ты готов?")
+    if message.from_user.id not in users:
+        users[message.from_user.id] = {'in_game': False,
+            'secret_number': None,
+            'attempts': 5,
+            'total_games': 0,
+            'wins': 0
+        }
+        await message.answer(
+            "Привет!Я бот который играет  с тобой угадай число\n, я загадываю любое число от 1 до 10 и говорю отгадал ты или нет,ты готов?")
+    else:
+        await message.answer(
+            "Привет!Я бот который играет  с тобой угадай число\n, я загадываю любое число от 1 до 10 и говорю отгадал ты или нет,ты готов?")
     
 
 
@@ -33,56 +40,66 @@ async def help(message: Message):
 
 @dp.message(Command(commands="stat"))
 async def process_stat_command(message: Message):
-    await message.answer(f"🎯Доступные попытки: {user['attempts']}(с платной версией 10)\n🎉Всего игр сыграно: {user['total_games']} \n🏆Побед: {user['wins']}")
-
+    print(message.from_user.id,users[message.from_user.id])
+    if users[message.from_user.id]['in_game']:
+        await message.answer(f"🎯Доступные попытки: {users[message.from_user.id]['attempts']}(с платной версией 10)\n🎉Всего игр сыграно: {users[message.from_user.id]['total_games'] + 1} \n🏆Побед: {users[message.from_user.id]['wins']}")
+    else:
+        await message.answer(f"🎯Доступные попытки: {users[message.from_user.id]['attempts']}(с платной версией 10)\n🎉Всего игр сыграно: {users[message.from_user.id]['total_games']} \n🏆Побед: {users[message.from_user.id]['wins']}")
 
 @dp.message(Command(commands="cancel"))
 async def process_cancel_command(message: Message):
-    global user
-    if user['in_game']:
-        user['in_game'] = False
+    global users
+    if users[message.from_user.id]['in_game']:
+        users[message.from_user.id]['in_game'] = False
+        users[message.from_user.id]['total_games'] += 1
         await message.answer("Эммм нуу окей,как хочешь")
     else:
         await message.answer("Чо ты там отменить пытаешься тупой,эволюцию себе отмени❤❤❤")
 
 @dp.message(F.text.lower().in_(['да', 'давай', 'сыграем', 'игра', 'играть', 'хочу играть']))
 async def process_positive_answer(message: Message):
-    global user,num
-    user['in_game'] = True
-    user['attempts'] = 5
+    global users,num
+    users[message.from_user.id]['in_game'] = True
+    users[message.from_user.id]['attempts'] = 5
     num = randomNum()
     await message.answer("Все я загадал,можешь отгадывать")
 
 def randomNum() -> int:
-    return random.randint(1,10)
+    return random.randint(1,100)
+
+@dp.message(F.text.lower().in_(['нет', 'не', 'не хочу', 'не буду']))
+async def process_negative_answer(message: Message):
+    if users[message.from_user.id]['in_game']:
+        await message.answer("Огадывай давай,не хочет он")
+    else:
+        await message.answer("Ну не хочешь как хочешь,твои проблемы")
 
 @dp.message()
 async def answer(message:Message):
     global num
-    if user['in_game']:
+    if users[message.from_user.id]['in_game']:
         if message.text.isdigit():
             if int(message.text) >= 0 and int(message.text) <= 100:
-                if user['attempts'] > 0:
+                if users[message.from_user.id]['attempts'] > 0:
                     if int(message.text) == num :
                         await message.answer("Молодец,угадал,просто тигр❤❤❤")
-                        user['in_game'] = False
-                        user['total_games'] += 1
-                        user['wins'] += 1
-                    else:
-                        await message.answer("Не угадал,попробуй еще😢😢😢")
-                        user['attempts'] -= 1
+                        users[message.from_user.id]['in_game'] = False
+                        users[message.from_user.id]['total_games'] += 1
+                        users[message.from_user.id]['wins'] += 1
+                    elif int(message.text) > num:
+                        await message.answer("Меньше давай,куда гонишь")
+                        users[message.from_user.id]['attempts'] -= 1
+                    elif int(message.text) < num:
+                        await message.answer("Не угадал, Больше нада")
+                        users[message.from_user.id]['attempts'] -= 1
                 else:
-                    await message.answer("У тебя попыток не осталось,ТУПОЙ,игра закончена")
-                    user['in_game'] = False
-                    user['total_games'] += 1
+                    await message.answer("У тебя попыток не осталось,НЕ ТУПОЙ,игра закончена,число было " + num)
+                    users[message.from_user.id]['in_game'] = False
+                    users[message.from_user.id]['total_games'] += 1
         else:
             await message.reply("А ну говори нормальным языком,ничего же не понятно")
-
-@dp.message(F.text.lower().in_(['нет', 'не', 'не хочу', 'не буду']))
-async def process_negative_answer(message: Message):
-    if user['in_game']:
-        await message.answer("Огадывай давай,не хочет он")
     else:
-        await message.answer("Ну не хочешь как хочешь,твои проблемы")
+        await message.reply("Я тебя не понял,ты хочешь играть или нет?")
+
 
 dp.run_polling(bot)
